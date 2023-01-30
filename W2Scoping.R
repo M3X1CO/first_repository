b7536a58fb43a47c78637d2e695ed1fb575792a0
Howar <howard.f64@rsu.ac.th>
2023-01-30 15:51:34 +0700

    1

--- W2Scoping.R
+++ W2Scoping.R
@@ -6,3 +6,29 @@ lm <- function(x) {x * x}
 f <- function(x, y) {
   x ^ 2 + y / z
 }
+
+make.power <- function(n) {
+  pow <- function(x) {
+    x^n
+  }
+  pow
+}
+
+cube <- make.power(3)
+square <- make.power(2)
+# call using
+#cube(3) or square(3)
+
+#whats in a functions environment
+ls(environment(cube))
+get("n", environment(cube))
+
+#lexical scoping vs dynamic scoping
+y <- 10
+f <- function(x) {
+  y <- 2
+  y ^ 2 + g(x)
+}
+g <- function(x) {
+  x * y
+}
